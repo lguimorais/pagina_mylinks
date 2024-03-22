137134ebe6a6ecb116e8d57ca5e5085fe72a317b
luis guilherme <tete42449@gmail.com>
2024-03-22 12:46:37 -0300

    img

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,18 @@
 function togglemode() {
- const html= document.documentElement
- 
-     // if(html.classList.contains('light')){
-     // html.classList.remove('light')
-     // }  else{html.classList.add('light')
-     // }
-
+    const html = document.documentElement
     html.classList.toggle('light')
-}
+     
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+   
+       //substituir a imagem
+       if(html.classlist.contains("light")) {
+        //se tiver light mode, adcionar a imagem light
+        img.setAttribute("src", "assets/avatar.png")
+       } else {
+        //set tiver sem light mode, manter imagem normal
+        img.setAttribute("src", "assets/avatar-light.png")
+   
+       }
+   }
+      
