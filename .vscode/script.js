782e79cc7fb0f28a7769701923b835d2d6c05f9c
luis guilherme <tete42449@gmail.com>
2024-03-22 12:48:04 -0300

    img

--- .vscode/script.js
+++ .vscode/script.js
@@ -8,10 +8,10 @@ function togglemode() {
        //substituir a imagem
        if(html.classlist.contains("light")) {
         //se tiver light mode, adcionar a imagem light
-        img.setAttribute("src", "assets/avatar.png")
+        img.setAttribute("src", "assets/avatar-light.png")
        } else {
         //set tiver sem light mode, manter imagem normal
-        img.setAttribute("src", "assets/avatar-light.png")
+        img.setAttribute("src", "assets/avatar.png")
    
        }
    }
