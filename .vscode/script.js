9bc85a789194603aa2befceeddf93059d850d2e6
luis guilherme <tete42449@gmail.com>
2024-03-23 16:40:47 -0300

    foto light

--- .vscode/script.js
+++ .vscode/script.js
@@ -5,14 +5,12 @@ function togglemode() {
     // pegar a tag img
     const img = document.querySelector("#profile img")
    
-       //substituir a imagem
+    //substituir a imagem
        if(html.classlist.contains("light")) {
-        //se tiver light mode, adcionar a imagem light
-        img.setAttribute("src", "assets/avatar-light.png")
+    //se tiver light mode, adcionar a imagem light
+        img.setAttribute("src","assets/avatar-light.png")
        } else {
-        //set tiver sem light mode, manter imagem normal
-        img.setAttribute("src", "assets/avatar.png")
-   
+    //set tiver sem light mode, manter imagem normal
+        img.setAttribute("src","assets/avatar.png")
        }
    }
-      
