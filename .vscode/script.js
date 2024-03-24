03fba4db9340bce05326b1a11f3930d8acc2422c
luis guilherme <tete42449@gmail.com>
2024-03-24 16:56:06 -0300

    melhoria de imagem

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,16 +1,16 @@
 function togglemode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
-     
-    // pegar a tag img
-    const img = document.querySelector("#profile img")
-   
-    //substituir a imagem
-       if(html.classlist.contains("light")) {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classlist.contains("light")) {
     //se tiver light mode, adcionar a imagem light
-        img.setAttribute("src","assets/avatar-light.png")
-       } else {
+    img.setAttribute("src", "assets/avatar-light.png")
+  } else {
     //set tiver sem light mode, manter imagem normal
-        img.setAttribute("src","assets/avatar.png")
-       }
-   }
+    img.setAttribute("src", "assets/avatar.png")
+  }
+}
